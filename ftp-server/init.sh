2109e920e3e186ae3154de617d642b77ae643b4b
ldonita <ldonita@student.21-school.ru>
2021-07-20 00:35:28 +0300

    ftps into virtual machine instead of docker volume..... todo fix!

--- ftp-server/init.sh
+++ ftp-server/init.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 env
+	(
+    echo 'LogLevel DEBUG2'; \
+    echo 'PermitRootLogin yes'; \
+    echo 'PasswordAuthentication yes'; \
+    echo 'Subsystem	sftp	internal-sftp'; \
+    echo 'Match User sftpuser1'; \
+    echo '\tChrootDirectory /static'; \
+    echo '\tForceCommand internal-sftp'; \
+    echo '\tPermitTunnel no'; \
+    echo '\tAllowAgentForwarding no'; \
+    echo 'AllowTcpForwarding no'; \
+    echo 'X11Forwarding no'; \
+  ) > /etc/ssh/sshd_config2 \
+  && mkdir -p /run/sshd;
 
-useradd -m '$FTP_USER' \
-	&& yes '$FTP_PASS' | passwd root;
-usermod -s /bin/bash '$FTP_USER';
+useradd -m ${FTP_USER} \
+	&& yes ${FTP_PASS} | passwd ${FTP_USER};
+usermod -s /bin/bash ${FTP_USER};
+
+chmod -R  o+r /static; 
+
+/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config2;
+
