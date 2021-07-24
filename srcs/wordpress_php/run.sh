f182123bc57dda5c8055915e932052b612645f86
ldonita <ldonita@student.21-school.ru>
2021-07-24 10:09:19 +0300

    db connect works from second time only...

--- srcs/wordpress_php/run.sh
+++ srcs/wordpress_php/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# wp core install --path=/static/wordpress --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root;
 
-mv /wordpress /static;
-
-# TODO:
-wp core install --path=/static/wordpress --url=$DOMAIN_NAME --title="$SITE_TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
-wp user create $EDITOR_USER $EDITOR_EMAIL --role=editor --user_pass=$EDITOR_PASSWORD --path=/static/wordpress --allow-root 
+# if [ ! -d /static/wordpress ]
+# then
+sleep 2;
+mv -f /wordpress /static
+wp core install --path=/static/wordpress --url=${DOMAIN_NAME} --title='${SITE_TITLE}' --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root --debug;
+wp user create ${EDITOR_USER} ${EDITOR_EMAIL} --role=editor --user_pass=${EDITOR_PASSWORD} --path=/static/wordpress --allow-root --debug;
+# fi;
 /usr/sbin/php-fpm7.3 -F;
